6162f1e8263d61619dc710bbfc5d902712c4133d
Nishant213 <nishantnadkarni@gmail.com>
2022-12-13 01:21:57 -0500

    Transaction code 1

--- src/main/java/Backend_Files/Transaction.java
+++ src/main/java/Backend_Files/Transaction.java
@@ -1,11 +1,17 @@
 package Backend_Files;
 
 public class Transaction {
-    public void toAccount(){
+    public void toAccount(String senderNo, String receiverNo, double amt){
 
     }
 
-    public void toSelf(){
+    public void toSelf(String accountNo, double amt){
 
+    }
+
+    public static void insert(Account account) {
+        String statement = "INSERT INTO ACCOUNT (ACCOUNTNUM,USERID,ACCOUNTTYPE,BALANCE) " +
+                "VALUES ("+account.getAccountNumber()+", "+account.getCustomer().getId()+", '"+account.getAccountType()+"', "+account.getBal()+");";
+        DatabaseController.getInstance().updateStatement(statement);
     }
 }
