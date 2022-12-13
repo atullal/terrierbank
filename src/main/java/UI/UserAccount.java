fc52ca6303d9f8240821650c0f3ebeaf3a468ca4
Atul Lal <atul.lal123@gmail.com>
2022-12-13 12:48:13 -0500

    View account

--- src/main/java/UI/UserAccount.java
+++ src/main/java/UI/UserAccount.java
@@ -3,16 +3,19 @@ package UI;/*
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JPanel.java to edit this template
  */
 
+import Account.Account;
+
 /**
  *
  * @author saisuryavarshith
  */
 public class UserAccount extends javax.swing.JPanel {
-
+    private Account account;
     /**
      * Creates new form savacc
      */
-    public UserAccount() {
+    public UserAccount(Account account) {
+        this.account = account;
         initComponents();
     }
 
@@ -66,6 +69,7 @@ public class UserAccount extends javax.swing.JPanel {
         jLabel5.setPreferredSize(new java.awt.Dimension(120, 30));
 
         jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setText(account.getAccountNumber());
         jLabel1.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
         jLabel1.setSize(new java.awt.Dimension(40, 20));
 
@@ -83,10 +87,12 @@ public class UserAccount extends javax.swing.JPanel {
         jLabel3.setSize(new java.awt.Dimension(40, 20));
 
         jLabel4.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel4.setText(account.getAccountType().toString());
         jLabel4.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
         jLabel4.setSize(new java.awt.Dimension(40, 20));
 
         jLabel6.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel6.setText(String.valueOf(account.getBal()));
         jLabel6.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
         jLabel6.setPreferredSize(new java.awt.Dimension(40, 20));
         jLabel6.setSize(new java.awt.Dimension(40, 20));
