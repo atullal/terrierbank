072f19d695caa5afddebab2d620f027d21e10c28
Nishant213 <nishantnadkarni@gmail.com>
2022-12-13 13:57:10 -0500

    Transaction code 5

--- src/main/java/UI/DepositForm.java
+++ src/main/java/UI/DepositForm.java
@@ -146,6 +146,7 @@ public class DepositForm extends javax.swing.JPanel {
         );
     }// </editor-fold>//GEN-END:initComponents
 
+
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
          // TODO add your handling code here:]
         System.out.println(jComboBox1.getSelectedItem());
@@ -156,6 +157,7 @@ public class DepositForm extends javax.swing.JPanel {
                 .setRightComponent(addUpdatePanel);
     }//GEN-LAST:event_jButton1ActionPerformed
 
+    // Back button
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
         UserViewAccounts addUpdatePanel = new UserViewAccounts();
